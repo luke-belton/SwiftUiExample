2842e2a8e019d37dd3a0c539ca95f687a94899c0
Luke Belton <58511679+luke-belton@users.noreply.github.com>
2022-09-30 11:56:37 +0100

    Initial Commit

---
+++ SwiftUiExample/SwiftUiExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiExampleApp.swift
+//  SwiftUiExample
+//
+//  Created by Luke Belton on 30/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
